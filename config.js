72d4b4c34f9ae73d60a2b26761c9b6ac20be10f5
waqarking55 <nasirhussain8888000@gmail.com>
2025-06-20 14:54:31 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "SHABAN-MD~LAsQUC5Z#LPee4OjQyycep1iiOQQoJcFb7jc6ehRc1TFjJhr1BxM",
 // add your Session Id
 AUTO_BIO: process.env.AUTO_BIO || "false", 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
